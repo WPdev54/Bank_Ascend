8050f305e95882c0316d13e65218da03e3a11345
CodeFusion Lab <wpdevolper54@gmail.com>
2024-07-06 20:19:54 +0600

    ascend

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">Welcome, s</header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
